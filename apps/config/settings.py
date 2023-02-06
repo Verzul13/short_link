db7c680b3ee836fd87cff988efac2ab616ff58b4
Verzul13 <alex.xxx_13@mail.ru>
2023-02-06 18:01:50 +0300

    TEST-7: fix CI tests(redis)

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": [os.environ.get('REDIS_URL', "redis://127.0.0.1:6379")],
+        "LOCATION": [os.environ.get('REDIS_URL', "redis://redis:6379")],
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             'CONNECTION_POOL_CLASS': 'redis.BlockingConnectionPool',
