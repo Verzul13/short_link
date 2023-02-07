77f3ebaf6cc4bbb5cf539874f4575333d7e3cddd
Verzul13 <alex.xxx_13@mail.ru>
2023-02-07 13:17:32 +0300

    TEST-8: fix pylint and add envs

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nq5!8)jk!dew3mij%ry-h0xg6$*ov!tbldy=&1(wgi@(lt108v'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG", default=1)))
